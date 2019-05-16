233b2a6abe43b0553a00913bada41cc4544e0d69
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2019-05-16 23:13:25 +0200

    #15 Write tests for handler

---
+++ bundles/org.openhab.binding.supla/src/test/java/org/openhab/binding/supla/handler/CloudDeviceHandlerTest.java
@@ -0,0 +1,198 @@
+package org.openhab.binding.supla.handler;
+
+import io.github.glytching.junit.extension.random.Random;
+import io.github.glytching.junit.extension.random.RandomBeansExtension;
+import org.eclipse.smarthome.config.core.Configuration;
+import org.eclipse.smarthome.core.library.types.OnOffType;
+import org.eclipse.smarthome.core.thing.Bridge;
+import org.eclipse.smarthome.core.thing.ChannelUID;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingRegistry;
+import org.eclipse.smarthome.core.thing.ThingStatus;
+import org.eclipse.smarthome.core.thing.ThingStatusDetail;
+import org.eclipse.smarthome.core.thing.ThingStatusInfo;
+import org.eclipse.smarthome.core.thing.ThingUID;
+import org.eclipse.smarthome.core.thing.binding.ThingHandlerCallback;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.openhab.binding.supla.internal.cloud.api.ChannelsCloudApi;
+import org.openhab.binding.supla.internal.cloud.api.ChannelsCloudApiFactory;
+import org.openhab.binding.supla.internal.cloud.api.CloudApiClientFactory;
+import org.openhab.binding.supla.internal.cloud.api.IoDevicesCloudApi;
+import org.openhab.binding.supla.internal.cloud.api.IoDevicesCloudApiFactory;
+import org.osgi.framework.BundleContext;
+import pl.grzeslowski.jsupla.api.generated.ApiClient;
+import pl.grzeslowski.jsupla.api.generated.ApiException;
+import pl.grzeslowski.jsupla.api.generated.model.Channel;
+import pl.grzeslowski.jsupla.api.generated.model.ChannelExecuteActionRequest;
+import pl.grzeslowski.jsupla.api.generated.model.ChannelFunction;
+import pl.grzeslowski.jsupla.api.generated.model.Device;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.util.List;
+import java.util.Optional;
+
+import static java.lang.String.valueOf;
+import static java.util.Arrays.asList;
+import static org.apache.commons.lang3.reflect.FieldUtils.writeField;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
+import static org.openhab.binding.supla.SuplaBindingConstants.SUPLA_DEVICE_CLOUD_ID;
+import static pl.grzeslowski.jsupla.api.generated.model.ChannelFunctionActionEnum.TURN_OFF;
+import static pl.grzeslowski.jsupla.api.generated.model.ChannelFunctionActionEnum.TURN_ON;
+import static pl.grzeslowski.jsupla.api.generated.model.ChannelFunctionEnumNames.CONTROLLINGTHEGATE;
+import static pl.grzeslowski.jsupla.api.generated.model.ChannelFunctionEnumNames.CONTROLLINGTHEROLLERSHUTTER;
+import static pl.grzeslowski.jsupla.api.generated.model.ChannelFunctionEnumNames.DIMMERANDRGBLIGHTING;
+import static pl.grzeslowski.jsupla.api.generated.model.ChannelFunctionEnumNames.LIGHTSWITCH;
+import static pl.grzeslowski.jsupla.api.generated.model.ChannelFunctionEnumNames.RGBLIGHTING;
+
+@SuppressWarnings("WeakerAccess")
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(RandomBeansExtension.class)
+class CloudDeviceHandlerTest {
+    CloudDeviceHandler handler;
+    @Mock Thing thing;
+    @Mock Bridge bridge;
+    @Mock ThingUID bridgeUid;
+    @Mock ThingRegistry thingRegistry;
+    @Mock BundleContext bundleContext;
+    @Mock CloudBridgeHandler bridgeHandler;
+    @Mock CloudApiClientFactory apiClientFactory;
+    @Mock ApiClient apiClient;
+    @Mock Configuration configuration;
+    @Mock ChannelsCloudApiFactory channelsCloudApiFactory;
+    @Mock IoDevicesCloudApiFactory ioDevicesCloudApiFactory;
+    @Mock ChannelsCloudApi channelsCloudApi;
+    @Mock IoDevicesCloudApi ioDevicesCloudApi;
+    @Mock Device device;
+    @Mock ThingHandlerCallback callback;
+
+    // Channels
+    @Mock Channel lightChannel;
+    @Random
+    @Min(1) @Max(100) int lightChannelId;
+    @Mock Channel rgbChannel;
+    @Random
+    @Min(1) @Max(100) int rgbChannelId;
+    @Mock Channel dimmerAndRgbChannel;
+    @Random
+    @Min(1) @Max(100) int dimmerAndRgbChannelId;
+    @Mock Channel rollerShutterChannel;
+    @Random
+    @Min(1) @Max(100) int rollerShutterChannelId;
+    @Mock Channel gateChannel;
+    @Random
+    @Min(1) @Max(100) int gateChannelId;
+    List<Channel> allChannels;
+
+    @Random String oAuthToken;
+    @Random
+    @Min(1) @Max(100) int cloudId;
+
+    ThingUID thingUID = new ThingUID("supla", "1337");
+
+    @Captor ArgumentCaptor<ChannelExecuteActionRequest> channelExecuteActionRequestCaptor;
+
+    @BeforeEach
+    void setUp() throws IllegalAccessException, ApiException {
+        setUpChannels();
+        setUpInternalInitialize();
+    }
+
+    void setUpChannels() throws ApiException {
+        allChannels = asList(lightChannel, rgbChannel, dimmerAndRgbChannel, rollerShutterChannel, gateChannel);
+        allChannels.forEach(channel -> given(channel.isHidden()).willReturn(false));
+        given(lightChannel.getFunction()).willReturn(new ChannelFunction().name(LIGHTSWITCH));
+        given(rgbChannel.getFunction()).willReturn(new ChannelFunction().name(RGBLIGHTING));
+        given(dimmerAndRgbChannel.getFunction()).willReturn(new ChannelFunction().name(DIMMERANDRGBLIGHTING));
+        given(rollerShutterChannel.getFunction()).willReturn(new ChannelFunction().name(CONTROLLINGTHEROLLERSHUTTER));
+        given(gateChannel.getFunction()).willReturn(new ChannelFunction().name(CONTROLLINGTHEGATE));
+
+        given(lightChannel.getId()).willReturn(lightChannelId);
+        given(rgbChannel.getId()).willReturn(rgbChannelId);
+        given(dimmerAndRgbChannel.getId()).willReturn(dimmerAndRgbChannelId);
+        given(rollerShutterChannel.getId()).willReturn(rollerShutterChannelId);
+        given(gateChannel.getId()).willReturn(gateChannelId);
+    }
+
+    void setUpInternalInitialize() throws ApiException, IllegalAccessException {
+        given(thing.getBridgeUID()).willReturn(bridgeUid);
+        given(thingRegistry.get(bridgeUid)).willReturn(bridge);
+        given(bridge.getHandler()).willReturn(bridgeHandler);
+        given(bridgeHandler.getOAuthToken()).willReturn(Optional.of(oAuthToken));
+        given(thing.getConfiguration()).willReturn(configuration);
+        given(configuration.get(SUPLA_DEVICE_CLOUD_ID)).willReturn(cloudId);
+        given(channelsCloudApiFactory.newChannelsCloudApi(oAuthToken)).willReturn(channelsCloudApi);
+        given(ioDevicesCloudApiFactory.newIoDevicesCloudApi(oAuthToken)).willReturn(ioDevicesCloudApi);
+        given(ioDevicesCloudApi.getIoDevice(eq(cloudId), any())).willReturn(device);
+        given(device.isConnected()).willReturn(true);
+        given(device.isEnabled()).willReturn(true);
+        given(device.getChannels()).willReturn(allChannels);
+        given(thing.getUID()).willReturn(thingUID);
+        given(channelsCloudApi.getChannel(anyInt(), any())).willAnswer(invocationOnMock -> {
+            int channelId = invocationOnMock.getArgument(0);
+            return allChannels.stream()
+                           .filter(channel -> channel.getId() == channelId)
+                           .findAny()
+                           .orElseThrow(IllegalArgumentException::new);
+        });
+
+        handler = new CloudDeviceHandler(thing, channelsCloudApiFactory, ioDevicesCloudApiFactory);
+        writeField(handler, "thingRegistry", thingRegistry, true);
+        writeField(handler, "callback", callback, true);
+        doAnswer(__ -> {
+            writeField(handler, "thing", thing, true);
+            return null;
+        }).when(callback).thingUpdated(any());
+        handler.internalInitialize();
+        verify(callback).statusUpdated(thing, new ThingStatusInfo(ThingStatus.ONLINE, ThingStatusDetail.NONE, null));
+    }
+
+    @Test
+    @DisplayName("should send request to Supla cloud to turn light ON")
+    void lightChannelOn() throws Exception {
+
+        // given
+        final ChannelUID lightChannelUID = findLightChannelUID();
+
+        // when
+        handler.handleOnOffCommand(lightChannelUID, OnOffType.ON);
+
+        // then
+        verify(channelsCloudApi).executeAction(channelExecuteActionRequestCaptor.capture(), eq(lightChannelId));
+        ChannelExecuteActionRequest value = channelExecuteActionRequestCaptor.getValue();
+        assertThat(value.getAction()).isEqualTo(TURN_ON);
+    }
+
+    @Test
+    @DisplayName("should send request to Supla cloud to turn light OFF")
+    void lightChannelOff() throws Exception {
+
+        // given
+        final ChannelUID lightChannelUID = findLightChannelUID();
+
+        // when
+        handler.handleOnOffCommand(lightChannelUID, OnOffType.OFF);
+
+        // then
+        verify(channelsCloudApi).executeAction(channelExecuteActionRequestCaptor.capture(), eq(lightChannelId));
+        ChannelExecuteActionRequest value = channelExecuteActionRequestCaptor.getValue();
+        assertThat(value.getAction()).isEqualTo(TURN_OFF);
+    }
+
+    ChannelUID findLightChannelUID() {
+        return new ChannelUID(thingUID, valueOf(lightChannelId));
+    }
+}
